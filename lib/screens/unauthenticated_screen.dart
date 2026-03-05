f0c3dec36b331f6dba8e5bf509361a3e70a2a487
Anupam Dahal <miracleanupam@gmail.com>
2026-03-05 17:02:51 -0500

    bg changes for unathenticated screen

--- lib/screens/unauthenticated_screen.dart
+++ lib/screens/unauthenticated_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adiary/compnents/alevated_button.dart';
 import 'package:adiary/constants.dart';
+import 'package:adiary/constants.dart' as constants;
 import 'package:flutter/material.dart';
 
 class UnauthenticatedScreen extends StatelessWidget {
@@ -13,33 +14,38 @@ class UnauthenticatedScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: PinkColors.shade100,
       appBar: AppBar(
+        flexibleSpace: constants.appBarBg,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Padding(
           padding: const EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 0.0),
           child: Text('Please Authenticate...'),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (isAuthenticating)
-                AlevatedButton(
-                    onPressed: () {},
-                    icon: Icons.more_horiz,
-                    text: 'Awaiting Authentication')
-              else
-                Column(
-                  children: <Widget>[
-                    AlevatedButton(
-                        onPressed: authenticate,
-                        icon: Icons.perm_device_information,
-                        text: 'Authenticate'),
-                  ],
-                ),
-            ],
+      body: Container(
+        decoration: constants.bgDecoration,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (isAuthenticating)
+                  AlevatedButton(
+                      onPressed: () {},
+                      icon: Icons.more_horiz,
+                      text: 'Awaiting Authentication')
+                else
+                  Column(
+                    children: <Widget>[
+                      AlevatedButton(
+                          onPressed: authenticate,
+                          icon: Icons.perm_device_information,
+                          text: 'Authenticate'),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
       ),
